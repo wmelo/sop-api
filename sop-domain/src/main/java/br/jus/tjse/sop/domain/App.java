a5774927e2e35885cf7ec761c7d1d30e86f419e9
17356 <17356@AN2DIDESI-ES04.tjse.jus.br>
2017-08-07 12:01:42 -0300

    Primeiro commit do projeto

---
+++ sop-domain/src/main/java/br/jus/tjse/sop/domain/App.java
@@ -0,0 +1,13 @@
+package br.jus.tjse.sop.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
